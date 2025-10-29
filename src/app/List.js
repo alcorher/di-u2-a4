8186428f0ba30bd92e3bfd344c46a53c3c3be723
Alcorher <111181871+alcorher@users.noreply.github.com>
2025-10-29 09:39:41 +0100

    ej 1 y 2 hechos

--- src/app/List.js
+++ src/app/List.js
@@ -1,6 +1,42 @@
 import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
+
+const químicos = people.filter(person => person.profession === 'químico');
+
+function listQuimicos() {
+  return químicos.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        conocido/a por {person.accomplishment}
+      </p>
+    </li>
+  );
+}
+
+const otros = people.filter(person => person.profession !== 'químico');
+function listOtros() {
+  return otros.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        conocido/a por {person.accomplishment}
+      </p>
+    </li>
+  );
+}
+
 export default function List() {
   const listItems = people.map(person =>
     <li key={person.id}>
@@ -18,7 +54,8 @@ export default function List() {
   return (
     <article>
       <h1>Científicos</h1>
-      <ul>{listItems}</ul>
+      <ul>{listQuimicos()}</ul>
+      <ul>{listOtros()}</ul>
     </article>
   );
 }
--- src/app/RecipeList.js
+++ src/app/RecipeList.js
@@ -1,9 +1,28 @@
 import { recipes } from './recipes.js';
 
+
+function listarRecetas() {
+  return recipes.map(recipe => (
+    <div key={recipe.id}>
+      <h2>{recipe.name}</h2>
+      <ul>
+        {recipe.ingredients.map(ingredient => (
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ));
+}
+
+
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recetas</h1>
+      {listarRecetas()}
     </div>
   );
 }
